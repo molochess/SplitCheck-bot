d46a3ac78daaa8e3555787a294cf278aa9c132b7
HOME TASK 1 <lilpowerfulann@gmail.com>
2019-11-19 07:41:15 +0300

    В общем, все, что есть

--- src/main/java/TelegramBot.java
+++ src/main/java/TelegramBot.java
@@ -1,24 +1,24 @@
+import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Locality;
+import org.telegram.abilitybots.api.objects.Privacy;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TelegramBot extends TelegramLongPollingBot {
-    private static final String BOT_TOKEN = "916831486:AAHRvkWsCz4WCu1sQo2ZsBIXt2U9XfDYhnk";
+public class TelegramBot extends AbilityBot {
     private static final String BOT_NAME = "@boleslavovich_bot";
+    private static final String BOT_TOKEN = System.getenv("TOKEN");
+    TelegramBot(DefaultBotOptions botOptions) {
+        super(BOT_TOKEN, BOT_NAME, botOptions);
+    }
 
-    private ReplyKeyboard getKeyboard(){
+ /*   private ReplyKeyboard getKeyboard(){
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         replyKeyboardMarkup.setOneTimeKeyboard(true);
 
@@ -33,20 +33,20 @@ public class TelegramBot extends TelegramLongPollingBot {
         keyboard.add(keyboardSecondRow);
         replyKeyboardMarkup.setKeyboard(keyboard);
         return replyKeyboardMarkup;
-    }
+    }*/
 
-
-    public static void main(String[] args){
+   public static void main(String[] args){
 
     ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
     try {
-        telegramBotsApi.registerBot(new TelegramBot());
+        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+        telegramBotsApi.registerBot(new TelegramBot(botOptions));
     } catch (TelegramApiException e){
         e.printStackTrace();
     }
 }
-    @Override
+   /* @Override
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
         SendMessage sendMessage = new SendMessage(message.getChatId(), message.getText());
@@ -62,7 +62,20 @@ public class TelegramBot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
 
-    }
+    }*/
+
+   public Ability hello() {
+       return Ability
+               .builder()
+               .name("pain")
+               .info("painful")
+               .input(0)
+               .locality(Locality.ALL)
+               .privacy(Privacy.PUBLIC)
+               .action( ctx -> silent.send("arrrrr", ctx.chatId()))
+               .post(ctx->silent.send("hate everything", ctx.chatId()))
+               .build();
+   }
 
     @Override
     public String getBotUsername() {
@@ -70,7 +83,13 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     @Override
+    public int creatorId() {
+        return 0;
+    }
+
+    @Override
     public String getBotToken() {
         return BOT_TOKEN;
     }
+
 }
